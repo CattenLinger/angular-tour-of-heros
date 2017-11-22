b8a3648a4592bb0523ffc4a143d0373359e74916
CattenLinger <lingerkong03@gmail.com>
2017-11-23 02:25:23 +0800

    It works

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,22 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 
+import { FormsModule } from '@angular/forms';
 
 import { AppComponent } from './app.component';
+import { HerosComponent } from './heros/heros.component';
+import { HeroDetailsComponent } from './hero-details/hero-details.component';
 
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HerosComponent,
+    HeroDetailsComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    FormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
