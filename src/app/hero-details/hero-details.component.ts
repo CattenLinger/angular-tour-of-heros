5d59b3c19c51088b30b461dd8ca17ec047448850
CattenLinger <lingerkong03@gmail.com>
2017-11-23 04:36:13 +0800

    Router feature added

--- src/app/hero-details/hero-details.component.ts
+++ src/app/hero-details/hero-details.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit, Input } from '@angular/core';
+import { ActivatedRoute } from "@angular/router";
+import { Location } from "@angular/common";
+
 import { Hero } from "../hero";
+
+import { HeroService } from "../hero.service";
+import { MessageService } from "../message.service";
 
 @Component({
   selector: 'app-hero-details',
@@ -8,11 +14,28 @@ import { Hero } from "../hero";
 })
 export class HeroDetailsComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    private route: ActivatedRoute,
+    private heroService: HeroService,
+    private location: Location,
+    private messageService: MessageService
+  ) { }
 
-  @Input() hero ; Hero;
+  @Input() hero; Hero;
 
   ngOnInit() {
+    this.getHero();
   }
 
+  getHero(): void {
+    const id = +this.route.snapshot.paramMap.get("id");
+    this.heroService.getHero(id).subscribe(hero => {
+      this.messageService.add(`HeroDetailsComponenet: data of hero id=${id} loaded`);
+      this.hero = hero;
+    })
+  }
+
+  goBack(): void {
+    this.location.back();
+  }
 }
