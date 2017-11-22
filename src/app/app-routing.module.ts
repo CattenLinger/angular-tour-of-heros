3b07d10d82af6e69a3370964531e91f4b8c6d45d
CattenLinger <lingerkong03@gmail.com>
2017-11-23 04:36:18 +0800

    Router feature added

---
+++ src/app/app-routing.module.ts
@@ -0,0 +1,24 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from "@angular/router";
+
+import { HeroesComponent } from "./heroes/heroes.component";
+import { HeroDetailsComponent } from "./hero-details/hero-details.component";
+import { DashboardComponent } from "./dashboard/dashboard.component";
+
+const routes : Routes = [
+  { path : "heroes", component : HeroesComponent},
+  { path : "dashboard", component : DashboardComponent},
+  { path : "details/:id", component : HeroDetailsComponent}
+]
+
+@NgModule({
+  imports : [ 
+    RouterModule.forRoot(routes)
+   ],
+  exports : [ 
+    RouterModule
+  ]
+})
+export class AppRoutingModule { 
+  
+}
